c61cd3498bbfc20d0e61e7c699a427891b95a3e2
= <=>
2023-02-07 19:17:48 +0100

    add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
@@ -15,7 +16,7 @@ import { UsersModule } from './users/users.module';
       database: 'reports',
       username: 'atemengue',
       password: 'regis1234',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
   ],
